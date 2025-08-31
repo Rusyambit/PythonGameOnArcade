c257ba35519938b67257a48aa176749e520a8806
Ruslan <ruslan@MacBook-Air-Ruslan.local>
2025-08-31 21:56:56 +0300

    test

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Platformer Game
+
+python -m arcade.examples.platform_tutorial.01_open_window
+"""
+import arcade
+
+# Constants 
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 700
+WINDOW_TITLE = "Death warrior"
+
+
+class GameView(arcade.Window):
+    def __init__(self):
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, fullscreen=False)
+
+        self.background_color = arcade.color.BURNT_UMBER
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        pass
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # The clear method should always be called at the start of on_draw.
+        # It clears the whole screen to whatever the background color is
+        # set to. This ensures that you have a clean slate for drawing each
+        # frame of the game.
+        self.clear()
+
+        # Code to draw other things will go here
+
+
+def main():
+    """Main function"""
+    window = GameView()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
